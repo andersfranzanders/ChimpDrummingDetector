24a33d9cccdaa4a975703597db0031ec99e308d9
anders <franz.anders@htwk-leipzig.de>
2022-03-15 09:37:31 +0100

    finished adapting program as python package to upload to PyPi

--- main.py
+++
@@ -1,8 +0,0 @@
-import chimp_detector
-
-path = "/home/anders/labpdgx/labp/3 Forschung/Remote Detection/Species Detection Audio/Audio_Ammie_Kalan/ARUdata_targetedchimpexcerpts/ARU4_20101009_114554_Jul01chimps.wav"
-
-output_df = chimp_detector.detect_drumming(path)
-
-
-print(output_df)
